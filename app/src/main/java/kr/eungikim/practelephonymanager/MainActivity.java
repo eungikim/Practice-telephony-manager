dcf355f05fb03f3ac49f17b386ecfbe53312787d
eungikimme <eungikim.me@gmail.com>
2019-09-30 22:33:37 +0900

    init project

---
+++ app/src/main/java/kr/eungikim/practelephonymanager/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.eungikim.practelephonymanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
